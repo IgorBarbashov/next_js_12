e8a70be8b64f478f56cd28084bf984b8315a7586
Igor Barbashov <bis200705@gmail.com>
2022-01-28 16:11:45 +0300

    feat: add ContextProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,14 @@
 import '../styles/libs/bootstrap.min.css';
 import '../styles/libs/style.css';
 import '../styles/globals.css';
+import { ContextProvider } from '../lib/context/contextProvider';
 
 function MyApp({ Component, pageProps }) {
-    return <Component { ...pageProps } />;
+    return (
+        <ContextProvider data = { pageProps.defaultData }>
+            <Component { ...pageProps } />
+        </ContextProvider>
+    );
 }
 
 export default MyApp;
