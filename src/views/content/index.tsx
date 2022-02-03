a5a7db91108bb54b4d4b84fea9f40cc0969aa9c4
Igor Barbashov <bis200705@gmail.com>
2022-02-04 01:48:29 +0300

    feat: make sider element on the ContentView not required

--- src/views/content/index.tsx
+++ src/views/content/index.tsx
@@ -2,29 +2,39 @@ import { FC, ReactElement } from 'react';
 
 interface IContentViewProps {
     content: ReactElement;
-    sider: ReactElement;
+    sider?: ReactElement;
 }
 
 export const ContentView: FC<IContentViewProps> = ({
     content,
-    sider,
-}) => (
-    <div className = 'sa4d25'>
-        <div className = 'container-fluid'>
-            <div className = 'row'>
-                <div className = 'col-xl-9 col-lg-8'>
-                    <div className = 'section3125'>
-                        <div className = 'la5lo1'>
-                            { content }
+    sider = null,
+}) => {
+    const contentWrapperClasses = sider === null
+        ? 'col-xl-12 col-lg-12'
+        : 'col-xl-9 col-lg-8';
+
+    return (
+        <div className = 'sa4d25'>
+            <div className = 'container-fluid'>
+                <div className = 'row'>
+                    <div className = { contentWrapperClasses }>
+                        <div className = 'section3125'>
+                            <div className = 'la5lo1'>
+                                { content }
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className = 'col-xl-3 col-lg-4'>
-                    <div className = 'right_side'>
-                        { sider }
-                    </div>
+                    { sider === null
+                        ? null
+                        : (
+                            <div className = 'col-xl-3 col-lg-4'>
+                                <div className = 'right_side'>
+                                    { sider }
+                                </div>
+                            </div>
+                        ) }
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
