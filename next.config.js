fd968bdd7d4af99299492b9813618678f96af794
Igor Barbashov <bis200705@gmail.com>
2022-02-09 16:46:44 +0300

    refactor: decompose next configuration

--- next.config.js
+++ next.config.js
@@ -1,51 +1,17 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-const { DuplicatesPlugin } = require('inspectpack/plugin');
+const { webpack } = require('./configs/next/webpack');
+const { images } = require('./configs/next/images');
 
-const nextConfig = {
-    webpack: (config, {
-        dev,
-    }) => {
-        if (!dev) {
-            config.plugins.push(
-                new DuplicatesPlugin({
-                    verbose: true,
-                    emitErrors: true,
-                }),
-            );
-        }
-
-        /* eslint-disable-next-line no-param-reassign */
-        config.performance = {
-            hints: 'warning',
-            maxAssetSize: 250000,
-            maxEntrypointSize: 250000,
-        };
-
-        return config;
-    },
+const customConfig = {
     reactStrictMode: true,
     swcMinify: true,
     optimizeFonts: true,
-    images: {
-        domains: ['placeimg.com', 'lectrum.io', 'images.unsplash.com'],
-        formats: ['image/avif', 'image/webp'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    },
+    webpack,
+    images,
 };
 
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            ...defaultConfig,
-            ...nextConfig,
-            compress: true,
-        };
-    }
-
-    return {
-        ...defaultConfig,
-        ...nextConfig,
-        compress: false,
-    };
-};
+module.exports = (phase, { defaultConfig }) => ({
+    ...defaultConfig,
+    ...customConfig,
+    compress: phase === PHASE_DEVELOPMENT_SERVER,
+});
