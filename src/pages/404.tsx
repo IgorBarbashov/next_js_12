07823e72e598eb4042990e0b4a61026b144f08c4
Igor Barbashov <bis200705@gmail.com>
2022-02-09 17:22:10 +0300

    feat: add default locale translate for error pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,32 @@
 import { ReactElement } from 'react';
-import { NextPage } from 'next';
+import { GetStaticProps, GetStaticPropsResult, NextPage } from 'next';
+import { SSRConfig, useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import { ErrorElement } from '~elements/error';
+import { getLocale } from '~utils';
 
-const NotFoundPage: NextPage = (): ReactElement => (
-    <>
-        <Head>
-            <title>Page not found</title>
-        </Head>
-        <ErrorElement
-            statusCode = { 404 }
-            title = 'The page you were looking for could not be found.'
-        />
-    </>
-);
+const NotFoundPage: NextPage = (): ReactElement => {
+    const { t } = useTranslation();
+    return (
+        <>
+            <Head>
+                <title>{ t('common:pageNotFoundTitle') }</title>
+            </Head>
+            <ErrorElement
+                statusCode = { 404 }
+                title = { t('common:pageNotFoundDescription') }
+            />
+        </>
+    );
+};
+
+export const getStaticProps: GetStaticProps<SSRConfig> = async (
+    context,
+): Promise<GetStaticPropsResult<SSRConfig>> => ({
+    props: {
+        ...await serverSideTranslations(getLocale(context), ['common']),
+    },
+});
 
 export default NotFoundPage;
